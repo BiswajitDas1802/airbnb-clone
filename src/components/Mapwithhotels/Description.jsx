0a1c67b7812eacd6cd3df183829f3333517b8d3a
BiswajitDas1802 <dasb1283@gmail.com>
2022-04-05 19:51:27 +0530

    added like button

--- src/components/Mapwithhotels/Description.jsx
+++ src/components/Mapwithhotels/Description.jsx
@@ -60,6 +60,7 @@ const BotDiv = styled.div`
 const Description = ({item}) => {
     const {title,description,location,star,price} = item
     const [winwidth,setWinwidth] = useState(false)
+    const [like,setLike] = useState(false)
     
     const windWid =   window.innerWidth
     useEffect(() => {
@@ -70,12 +71,15 @@ const Description = ({item}) => {
       }
     }, [])
     
+    const handleLike=()=>{
+        setLike(!like)
+    }
 
 
   return (
     <DescriptionDiv>
         <div >
-          <div><P>{title}</P><i class="far fa-heart"></i> </div>
+          <div><P>{title}</P>{like?<svg onClick={handleLike} xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 512 512" height="23px"><path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"/></svg>:<i onClick={handleLike} class="far fa-heart"></i>} </div>
           <H3>{windWid<1300?location.substring(0,30)+"...":location}</H3>
         </div>
         <hr/>
