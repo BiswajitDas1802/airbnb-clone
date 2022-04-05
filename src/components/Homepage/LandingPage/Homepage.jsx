11251a561f964c57c30c82ab473a1e0f90e2c690
AlokSharma01 <aloksj788@gmail.com>
2022-04-05 18:29:48 +0530

    footer added

--- src/components/Homepage/LandingPage/Homepage.jsx
+++ src/components/Homepage/LandingPage/Homepage.jsx
@@ -35,29 +35,29 @@ export const Homepage = () => {
                 <div className={Styles.single_card}>
                     <img className={Styles.card_image} src="https://a0.muscache.com/im/pictures/73250991-433e-4950-b7d1-59bba711bb57.jpg?im_w=480" width="100%"></img>
                     <div style={{backgroundColor:"#bc1a6e"}} className={Styles.card_color}>
-                        <p className={Styles.destination_title}>Mussoorie</p>
-                        <p className={Styles.distance}>223 kilometers away</p>
+                        <p className={Styles.destination_title}>Lonavla</p>
+                        <p className={Styles.distance}>66 kilometers away</p>
                     </div>
                 </div>
                 <div className={Styles.single_card}>
                 <img className={Styles.card_image} src="https://a0.muscache.com/im/pictures/aef20929-0d6a-40e7-8ac9-321ff0edf8c9.jpg?im_w=480" width="100%"></img>
                 <div style={{backgroundColor:"#cc2d4a"}} className={Styles.card_color}>
-                        <p className={Styles.destination_title}>Manali</p>
-                        <p className={Styles.distance}>405 kilometers away</p>
+                        <p className={Styles.destination_title}>Alibagh</p>
+                        <p className={Styles.distance}>37 kilometers away</p>
                     </div>
                 </div>
                 <div className={Styles.single_card}>
                 <img className={Styles.card_image} src="https://a0.muscache.com/im/pictures/57d3fdbd-9dde-4b6f-98af-64d38d6ffd02.jpg?im_w=480" width="100%"></img>
                 <div style={{backgroundColor:"#d93b30"}} className={Styles.card_color}>
-                        <p className={Styles.destination_title}>Kasauli</p>
-                        <p className={Styles.distance}>257 kilometers away</p>
+                        <p className={Styles.destination_title}>Calangute</p>
+                        <p className={Styles.distance}>396 kilometers away</p>
                     </div>
                 </div>
                 <div className={Styles.single_card}>
                 <img className={Styles.card_image} src="https://a0.muscache.com/im/pictures/cccdc2e6-e6dc-4706-b9d4-ee2a80115703.jpg?im_w=480" width="100%"></img>
                 <div style={{backgroundColor:"#de3151"}} className={Styles.card_color}>
-                        <p className={Styles.destination_title}>Haldwani</p>
-                        <p className={Styles.distance}>235 kilometers away</p>
+                        <p className={Styles.destination_title}>Karjat</p>
+                        <p className={Styles.distance}>53 kilometers away</p>
                     </div>
                 </div>
 
