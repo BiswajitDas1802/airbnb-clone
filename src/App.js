238550e533a5239d3875de9378d00b0fffbed560
BiswajitDas1802 <dasb1283@gmail.com>
2022-04-06 22:30:46 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -11,19 +11,16 @@ import { BookHotel } from './components/BookHotel/BookHotel';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Routes>
         <Route path="/" element={<Homepage/>}/>
         <Route  path="/lonavla" element={<MapwithHotels/>}/>
         <Route path="bookhotel" element={<BookHotel/>}/>
       </Routes>
-=======
 
         <Homepage/>
 
       {/* <MapwithHotels/> */}
 
->>>>>>> 76f8894c0296af43c8f183d3d2f19c63a1685f74
     </div>
   );
 }
